e1fb68d73c317cf39f1ecda35fb363cc65402078
이진호 <jhlee8@cookapps.com>
2025-04-21 12:28:08 +0900

    SQLITE_HAS_CODEC define add

--- Plugins/sqlite-amalgamation/sqlite3.h
+++ Plugins/sqlite-amalgamation/sqlite3.h
@@ -30,6 +30,9 @@
 ** the version number) and changes its name to "sqlite3.h" as
 ** part of the build process.
 */
+
+#define SQLITE_HAS_CODEC 1 // 암호화 사용
+
 #ifndef SQLITE3_H
 #define SQLITE3_H
 #include <stdarg.h>     /* Needed for the definition of va_list */
